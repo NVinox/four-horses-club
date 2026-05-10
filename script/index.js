9f3b966b48225d2c127cd872b2da01a314368e15
Sergey Klimov <93241730+NVinox@users.noreply.github.com>
2026-05-10 18:44:19 +0700

    feat: реализовал логику карусели

--- script/index.js
+++ script/index.js
@@ -1,7 +1,10 @@
 import { RunningLine } from "./RunningLine.js";
+import { Slider } from "./Slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const runningLine = new RunningLine();
+	const slider = new Slider("members");
 
 	runningLine.init();
+	slider.init();
 });
